2283efdf3510547983b50354afaad5ad371b3639
Krzysztof Górzyński <szy162@gmail.com>
2023-02-04 10:28:01 +0100

    initialize project for bike api

---
+++ bike/src/main/java/com/github/szysza26/bikecatalog/bike/BikeApplication.java
@@ -0,0 +1,13 @@
+package com.github.szysza26.bikecatalog.bike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikeApplication.class, args);
+	}
+
+}
---
+++ bike/src/test/java/com/github/szysza26/bikecatalog/bike/BikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.szysza26.bikecatalog.bike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
