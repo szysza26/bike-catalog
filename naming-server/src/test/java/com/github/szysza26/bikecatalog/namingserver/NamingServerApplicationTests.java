eb4a02fe5dc753e79ec3f29863a61bae9d5ce962
Krzysztof Górzyński <szy162@gmail.com>
2023-02-28 19:08:06 +0100

    configuring eureka nameserver

---
+++ naming-server/src/test/java/com/github/szysza26/bikecatalog/namingserver/NamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.szysza26.bikecatalog.namingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
